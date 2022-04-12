8f23dbeae1472a853cc39aafca6e7d8777d91475
Aakash Gupta <f20180887@pilani.bits-pilani.ac.in>
2022-04-12 18:18:19 +0530

    Offset Calculation Done

--- Compiler/IRGenerator.c
+++ Compiler/IRGenerator.c
@@ -3,6 +3,8 @@
 #include <assert.h>
 
 Trie* localSymbolTable;
+int localOffset;
+int globalOffset = 0;
 
 IRInsList* mergeLists(IRInsList* left, IRInsList* right)
 {
@@ -62,16 +64,33 @@ IRInsList* recurseiveGenFuncCode(ASTNode* stmt)
 	}
 }
 
+void fillOffsets(ASTNode* vars, FuncEntry* entry)
+{
+	while (vars)
+	{
+		VariableEntry* varEntry = vars->derived_type->structure;
+		varEntry->offset = localOffset;
+		varEntry->isGlobal = vars->isGlobal;
+		localOffset += varEntry->isGlobal ? 0 : vars->derived_type->width;
+		globalOffset += varEntry->isGlobal ? vars->derived_type->width : 0;
+
+		vars = vars->sibling;
+	}
+}
+
 IRInsList* generateFuncCode(ASTNode* funcNode)
 {
 	// get symbol table
 	TypeLog* mediator = trie_getRef(globalSymbolTable, funcNode->token->lexeme)->entry.ptr;
 	FuncEntry* funcEntry = mediator->structure;
 	localSymbolTable = funcEntry->symbolTable;
+	localOffset = 0;
 
 	logIt("Generating code for function: %s and its symbol symbol is found at adress: %p\n", funcEntry->name, localSymbolTable);
 
 	// Iterate over statements
-	// TODO: Check if offsets are calculated
+	fillOffsets(funcNode->children[0], funcEntry);
+	fillOffsets(funcNode->children[1], funcEntry);
+	fillOffsets(funcNode->children[2]->children[1], funcEntry);
 	return recurseiveGenFuncCode(funcNode->children[2]->children[2]);
 }
